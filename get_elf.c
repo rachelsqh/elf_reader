221b410b345b2c682e0d687e6123283cfd7e3716
rachel.sun <sqhxhg@163.com>
2020-10-22 05:04:28 +0800

    modify macro def

--- get_elf.c
+++ get_elf.c
@@ -6,66 +6,112 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 
-#define sec_type(sh_type)(\
-		(SHT_PROGBITS == sh_type)?\
-			"progbits":(\
-		(SHT_SYMTAB == sh_type) ?\
-			"symtab":(\
-		(SHT_STRTAB == sh_type) ?\
-			"strtab":(\
-	        (SHT_RELA == sh_type) ?\
-			"rela":(\
-	        (SHT_HASH == sh_type) ?\
-			"hash":(\
-	        (SHT_DYNAMIC == sh_type) ? \
-			"dynamic":(\
-	        (SHT_NOTE == sh_type) ? \
-			"note":(\
-	        (SHT_NOBITS == sh_type) ? \
-			"nobits":(\
-	        (SHT_REL == sh_type) ? \
-			"rel": (\
-	        (SHT_SHLIB == sh_type) ? \
-			"shlib": (\
-	        (SHT_DYNSYM == sh_type) ? \
-			"dynsym":(\
-	        (SHT_LOPROC == sh_type) ? \
-			"loproc":(\
-	        (SHT_HIPROC == sh_type) ? \
-			"hiproc":(\
-	        (SHT_LOUSER == sh_type) ? \
-			"louser":(\
-	        (SHT_HIUSER == sh_type) ? \
-			"hiuser":"none")))))))))))))))
+#define file_type(f_types) (\
+		(ET_NONE == f_types) ? "NONE" : (\
+		(ET_REL  == f_types) ? "REL"  : (\
+		(ET_EXEC == f_types) ? "EXEC" : (\
+		(ET_DYN  == f_types) ? "DYN" : (\
+		(ET_CORE == f_types) ? "CORE": (\
+		(ET_LOPROC == f_types) ? "LOPROC" : (\
+		(ET_HIPROC == f_types) ? "HIPROC" : "UNKNOWN")))))))
 
+
+
+#define sh_type(s_type) (\
+		(SHT_PROGBITS == s_type)?\
+			"PROGBITS":(\
+		(SHT_SYMTAB == s_type) ?\
+			"SYMTAB":(\
+		(SHT_STRTAB == s_type) ?\
+			"STRTAB":(\
+	        (SHT_RELA == s_type) ?\
+			"RELA":(\
+	        (SHT_HASH == s_type) ?\
+			"HASH":(\
+	        (SHT_DYNAMIC == s_type) ? \
+			"SYNAMIC":(\
+	        (SHT_NOTE == s_type) ? \
+			"NOTE":(\
+	        (SHT_NOBITS == s_type) ? \
+			"NOBITS":(\
+	        (SHT_REL == s_type) ? \
+			"REL": (\
+	        (SHT_SHLIB == s_type) ? \
+			"SHLIB": (\
+	        (SHT_DYNSYM == s_type) ? \
+			"SYNSYM":(\
+	        (SHT_LOPROC == s_type) ? \
+			"LOPROC":(\
+	        (SHT_HIPROC == s_type) ? \
+			"HIPROC":(\
+	        (SHT_LOUSER == s_type) ? \
+			"LOUSER":(\
+	        (SHT_HIUSER == s_type) ? \
+			"HIUSER":"UNKNOWN")))))))))))))))
+
+#define sh_flags(sh_flags) (\
+		(SHF_WRITE == sh_flags) ? "W" : (\
+		(SHF_ALLOC == sh_flags) ? "A" : (\
+		(SHF_EXECINSTR == sh_flags) ? "E" : (\
+		((SHF_WRITE | SHF_ALLOC | SHF_EXECINSTR) == sh_flags) ? "WAE" : (\
+		((SHF_WRITE | SHF_ALLOC ) == sh_flags) ? "WA" : (\
+		((SHF_WRITE | SHF_EXECINSTR ) == sh_flags) ? "WE" : (\
+		((SHF_ALLOC | SHF_EXECINSTR ) == sh_flags) ? "AE" : "UNKNOWN")))))))
+		       //	(\
+		//(SHF_RELA_LIVEPATCH == sh_flags) ? "LIVEPATCH" : (\
+	//	(SHF_RO_AFTER_INIT  == sh_flags) ? "RO_AFTER_INIT" : (\
+	//	(SHF_MASKPROC       == sh_flags) ? "MASKPROC":"UNKNOWN"))))))
 
 #define ph_type(ph_type)(\
 		(PT_LOAD == ph_type)?\
-			"load":(\
+			"LOAD":(\
 		(PT_DYNAMIC == ph_type) ?\
-			"dynamic":(\
+			"DYNAMIC":(\
 		(PT_INTERP == ph_type) ?\
-			"interp":(\
+			"INTERP":(\
 	        (PT_NOTE == ph_type) ?\
-			"note":(\
+			"NOTE":(\
 	        (PT_SHLIB == ph_type) ?\
-			"shlib":(\
+			"SHLIB":(\
 	        (PT_PHDR == ph_type) ? \
-			"phdr":(\
+			"PHDR":(\
 	        (PT_TLS == ph_type) ? \
-			"tls":(\
+			"TLS":(\
 	        (PT_LOOS == ph_type) ? \
-			"loop":(\
+			"LOOP":(\
 	        (PT_HIOS == ph_type) ? \
-			"hios": (\
+			"HIOS": (\
 	        (PT_LOPROC == ph_type) ? \
-			"loproc": (\
+			"LOPROC": (\
 	        (PT_HIPROC == ph_type) ? \
-			"hiproc":(\
+			"HIPROC":(\
 	        (PT_GNU_EH_FRAME == ph_type) ? \
-			"eh_frame":(\
+			"EH_FRAME":(\
 	        (PT_GNU_STACK == ph_type) ? \
-			"stack":"none")))))))))))))
+			"STACK":"UNKNOWN")))))))))))))
+#define ph_flags(p_flags) (\
+	((PF_W | PF_X | PF_R) == p_flags) ? "R W X" \
+        	: (\
+	((PF_W | PF_X) == p_flags) ? "W X" : (\
+	((PF_R | PF_X) == p_flags) ? "R X" : (\
+	((PF_R | PF_W) == p_flags) ? "R W" : (\
+	(PF_W == p_flags) ? "W" : (\
+	(PF_R == p_flags) ? "R" : (\
+	(PF_R == p_flags) ? "X" : "UNKNOWN")))))))
+
+
+
+#define sym_type(s_type) (\
+		(STT_NOTYPE == s_type) ? "NOTYPE" : (\
+		(STT_OBJECT == s_type) ? "OBJECT" : (\
+		(STT_FUNC == s_type) ? "FUNC" : (\
+		(STT_SECTION == s_type) ? "SECTION" : (\
+		(STT_FILE == s_type) ? "FILE" : (\
+		(STT_LOPROC == s_type) ? "LOPROC" : (\
+		(STT_HIPROC == s_type) ? "HIPROC" : "NOTYPE")))))))
+
+//#define 
+
 
 
 
@@ -81,7 +127,7 @@ static void dsym_info(Elf64_Ehdr *ehdr,void **buf,Elf64_Off sh_offset,Elf64_Xwor
 	shdr = (Elf64_Shdr *)&addr[ehdr->e_shoff + ehdr->e_shstrndx * sizeof(Elf64_Shdr)];
 	str_offset = shdr->sh_offset;	
 	size = sh_size / sizeof(Elf64_Sym);
-	printf("SYMBOL TABLE:entrys = %d\n",size);
+	printf("\nSYMBOL TABLE:entrys = %d\n",size);
 	printf("st_name\tst_info\tst_other\tst_shndx\tst_value\tst_size\n");
 	for(i = 0;i < size;i++){
 		sym = (Elf64_Sym *)&addr[sh_offset + i * sizeof(Elf64_Sym)];
@@ -115,7 +161,7 @@ static void sym_info(Elf64_Ehdr *ehdr,void **buf,Elf64_Off sh_offset,Elf64_Xword
 	shdr = (Elf64_Shdr *)&addr[ehdr->e_shoff + ehdr->e_shstrndx * sizeof(Elf64_Shdr)];
 	str_offset = shdr->sh_offset;	
 	size = sh_size / sizeof(Elf64_Sym);
-	printf("SYMBOL TABLE:entrys = %d\n",size);
+	printf("\nSYMBOL TABLE:entrys = %d\n",size);
 	printf("st_name\tst_info\tst_other\tst_shndx\tst_value\tst_size\n");
 	for(i = 0;i < size;i++){
 		sym = (Elf64_Sym *)&addr[sh_offset + i * sizeof(Elf64_Sym)];
@@ -149,7 +195,7 @@ static void print_shdr(void **buf,Elf64_Ehdr *ehdr)
 	str_offset = shdr->sh_offset;	
 	for(i = 0;i < ehdr->e_shnum;i++){
 		shdr = (Elf64_Shdr *)&addr[ehdr->e_shoff + i * sizeof(Elf64_Shdr)];
-		printf("[%d]\t%-8s\t%-8s\t0x%016lx\t%016lx\t%016lx\t%08x\t%08x\t%16lx\t%016lx\n",i,(char *)&addr[str_offset + shdr->sh_name],sec_type(shdr->sh_type),shdr->sh_flags,shdr->sh_offset,shdr->sh_size,shdr->sh_link,shdr->sh_info,shdr->sh_addralign,shdr->sh_entsize);	
+		printf("[%d]  %-16s  %-8s  0x%08s  %016lx  %016lx  %08x  %08x  %16lx  %016lx\n",i,(char *)&addr[str_offset + shdr->sh_name],sh_type(shdr->sh_type),sh_flags(shdr->sh_flags),shdr->sh_offset,shdr->sh_size,shdr->sh_link,shdr->sh_info,shdr->sh_addralign,shdr->sh_entsize);	
 	}
 
 }
@@ -157,18 +203,36 @@ static void print_shdr(void **buf,Elf64_Ehdr *ehdr)
 static void print_phdr(void **buf, Elf64_Ehdr *ehdr)
 {
 	Elf64_Phdr	*phdr = NULL;
+	Elf64_Shdr	*shdr = NULL;
+	char *addr = (char *)*buf;
 	int ret = 0;
-	int i = 0;
+	int i = 0,j = 0;
+	int str_offset = 0;
 
-	printf("\t Program header table e_phnum = %x\n",ehdr->e_phnum);
+	shdr = (Elf64_Shdr *)&addr[ehdr->e_shoff + ehdr->e_shstrndx * sizeof(Elf64_Shdr)];
+	str_offset = shdr->sh_offset;	
+	printf("\n Program header table e_phnum = %x\n",ehdr->e_phnum);
 
-	printf("index\ttype\toffset\tvirtual address\t physical address\t size in file\tsize in mem\talign\n");
+	printf("index  type flags offset  virtual address   physical address   size in file  size in mem  align\n");
 	
 	for(i = 0;i < ehdr->e_phnum;i++){
 		phdr = ((Elf64_Phdr *)(&(((char *)(*buf))[ehdr->e_phoff + i * sizeof(Elf64_Phdr)])));
-		printf("[%d]\t%08s\t0x%08x\t%016lx\t%016lx\t%016lx\t%016lx\t%016lx\t%016lx\n",i,ph_type(phdr->p_type),phdr->p_flags,phdr->p_offset,phdr->p_vaddr,phdr->p_paddr,phdr->p_filesz,phdr->p_memsz,phdr->p_align);	
+		printf("[%d]  %08s  0x%08s  0x%016lx  0x%016lx  0x%016lx  0x%016lx  0x%016lx  0x%016lx\n",i,ph_type(phdr->p_type),ph_flags(phdr->p_flags),phdr->p_offset,phdr->p_vaddr,phdr->p_paddr,phdr->p_filesz,phdr->p_memsz,phdr->p_align);	
 	}
-
+	
+	printf("\nSection to Segment mapping:\n");
+	printf("Segment : Sections...\n");
+	for(i = 0;i < ehdr->e_phnum;i++){
+		printf("[%02d]\t",i);
+		phdr = ((Elf64_Phdr *)(&(((char *)(*buf))[ehdr->e_phoff + i * sizeof(Elf64_Phdr)])));
+		for(j = 0;j < ehdr->e_shnum;j++){
+			shdr = (Elf64_Shdr *)&addr[ehdr->e_shoff + j * sizeof(Elf64_Shdr)];
+			if((shdr->sh_offset >=  phdr->p_offset) && (shdr->sh_offset < (phdr->p_offset + phdr->p_filesz))){
+				printf("%s,",(char *)&addr[str_offset + shdr->sh_name]);
+			}
+		}
+		printf("\n");
+	}	
 }
 
 static void print_ehdr(Elf64_Ehdr *ehdr)
@@ -230,9 +294,9 @@ int main(int argc,char *argv[])
 	print_ehdr(&ehdr);
 	print_shdr(&buf,&ehdr);
 	print_phdr(&buf,&ehdr);
-	print_sym(&buf,&ehdr);
-	print_dsym(&buf,&ehdr);
-	printf("buf +size=%lx\n",buf + len);
+	//print_sym(&buf,&ehdr);
+	//print_dsym(&buf,&ehdr);
+	//printf("buf +size=%lx\n",buf + len);
 	munmap(buf,len);
 	close(fd);
 	return 0;
