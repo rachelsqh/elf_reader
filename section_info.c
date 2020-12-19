6d60d1fd35be877ef7cfeb4af6d315e0261b186f
root <root@rachel.sun>
2020-12-20 07:46:09 +0800

    modify section_info code

--- section_info.c
+++ section_info.c
@@ -7,48 +7,244 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 
-#define sh_type(s_type) (\
-		(SHT_PROGBITS == s_type)?\
-			"PROGBITS":(\
-		(SHT_SYMTAB == s_type) ?\
-			"SYMTAB":(\
-		(SHT_STRTAB == s_type) ?\
-			"STRTAB":(\
-	        (SHT_RELA == s_type) ?\
-			"RELA":(\
-	        (SHT_HASH == s_type) ?\
-			"HASH":(\
-	        (SHT_DYNAMIC == s_type) ? \
-			"SYNAMIC":(\
-	        (SHT_NOTE == s_type) ? \
-			"NOTE":(\
-	        (SHT_NOBITS == s_type) ? \
-			"NOBITS":(\
-	        (SHT_REL == s_type) ? \
-			"REL": (\
-	        (SHT_SHLIB == s_type) ? \
-			"SHLIB": (\
-	        (SHT_DYNSYM == s_type) ? \
-			"SYNSYM":(\
-	        (SHT_LOPROC == s_type) ? \
-			"LOPROC":(\
-	        (SHT_HIPROC == s_type) ? \
-			"HIPROC":(\
-	        (SHT_LOUSER == s_type) ? \
-			"LOUSER":(\
-	        (SHT_HIUSER == s_type) ? \
-			"HIUSER":"UNKNOWN")))))))))))))))
+struct type_hash {
+	unsigned int index;
+	char *name;
+};
 
-#define sh_flags(sh_flags) (\
-		(SHF_WRITE == sh_flags) ? "W" : (\
-		(SHF_ALLOC == sh_flags) ? "A" : (\
-		(SHF_EXECINSTR == sh_flags) ? "E" : (\
-		((SHF_WRITE | SHF_ALLOC | SHF_EXECINSTR) == sh_flags) ? "WAE" : (\
-		((SHF_WRITE | SHF_ALLOC ) == sh_flags) ? "WA" : (\
-		((SHF_WRITE | SHF_EXECINSTR ) == sh_flags) ? "WE" : (\
-		((SHF_ALLOC | SHF_EXECINSTR ) == sh_flags) ? "AE" : "UNKNOWN")))))))
+struct type_hash sh_type_index[] = {
+	{
+		.index = SHT_NULL,
+		.name = "NULL",
+	},
+	{
+		.index = SHT_PROGBITS,
+		.name = "PROGBITS",
+	},
+	{
+		.index = SHT_SYMTAB,
+		.name = "SYMTAB",
+	},
+	{
+		.index = SHT_STRTAB,
+		.name = "STRTAB",
+	},
+	{
+		.index = SHT_RELA,
+		.name = "RELA",
+	},
+	{
+		.index = SHT_HASH,
+		.name = "HASH",
+	},
+	{
+		.index = SHT_DYNAMIC,
+		.name = "DYNAMIC",
+	},
+	{
+		.index = SHT_NOTE,
+		.name = "NOTE",
+	},
+	{
+		.index = SHT_NOBITS,
+		.name = "NOBITS",
+	},
+	{
+		.index = SHT_REL,
+		.name = "REL",
+	},
+	{
+		.index = SHT_SHLIB,
+		.name = "SHLIB",
+	},
+	{
+		.index = SHT_DYNSYM,
+		.name = "DYNSYM",
+	},
+	{
+		.index = SHT_INIT_ARRAY,
+		.name = "INIT_ARRAY",
+	},
+	{
+		.index = SHT_FINI_ARRAY,
+		.name = "FINI_ARRAY",
+	},
+	{
+		.index = SHT_PREINIT_ARRAY,
+		.name = "PREINIT_ARRAY",
+	},
+	{
+		.index = SHT_GROUP,
+		.name = "GROUP",
+	},
+	{
+		.index = SHT_SYMTAB_SHNDX,
+		.name = "GROUP",
+	},
+	{
+		.index = SHT_NUM,
+		.name = "NUM",
+	},
+	{
+		.index = SHT_LOOS,
+		.name = "LOOS",
+	},
+	{
+		.index = SHT_GNU_ATTRIBUTES,
+		.name = "GNU_ATTRIBUTES",
+	},
+	{
+		.index = SHT_GNU_HASH,
+		.name = "GNU_HASH",
+	},
+	{
+		.index = SHT_GNU_LIBLIST,
+		.name = "GNU_LIBLIST",
+	},
+	{
+		.index = SHT_CHECKSUM,
+		.name = "CHECKSUM",
+	},
+	{
+		.index = SHT_LOSUNW,
+		.name = "LOSUNW",
+	},
+	{
+		.index = SHT_SUNW_move,
+      	        .name = "SUNW_move",
+	},
+	{
+		.index = SHT_SUNW_COMDAT,
+		.name = "SUNW_COMDAT",
+	},
+	{
+		.index = SHT_SUNW_syminfo,
+		.name = "SUNW_syminfo;=",
+	},
+	{
+		.index = SHT_GNU_verdef,
+		.name = "GNU_verdef",
+	},
+	{
+		.index = SHT_GNU_verneed,
+		.name = "GNU_verneed",
+	},
+	{
+		.index = SHT_GNU_versym,
+		.name = "GNU_versym",
+	},
+	{
+		.index = SHT_HISUNW,
+		.name = "HISUNW",
+	},
+	{
+		.index = SHT_HIOS,
+		.name = "HIOS",
+	},
+	{
+		.index = SHT_LOPROC,
+		.name = "LOPROC",
+	},
+	{
+		.index = SHT_HIPROC,
+		.name = "HIPROC",
+	},
+	{
+		.index = SHT_LOUSER,
+		.name = "LOUSER",
+	},
+	{
+		.index = SHT_HIUSER,
+		.name = "HIUSER",
+	},
+	{}
+	};
+
+#define sh_type(s_type) ({ \
+			int size_array = sizeof(sh_type_index)/sizeof(sh_type_index[0]),i = 0; \
+			for(i = 0;i < size_array;i++)\
+				if(sh_type_index[i].index == s_type)\
+					break;\
+			(i == size_array) ? "NULL" : sh_type_index[i].name \
+			;})
 
 
+struct type_hash sh_flags_index[] = {
+	{
+		.index = SHF_WRITE,
+		.name = "W",
+	},	
+	{
+		.index = SHF_ALLOC,
+		.name = "A",
+	},
+	{
+		.index = SHF_EXECINSTR,
+		.name = "X",
+	},
+	{
+		.index = SHF_MERGE,
+		.name = "M",
+	},
+	{
+		.index = SHF_STRINGS,
+		.name = "S",
+	},
+	{
+		.index = SHF_INFO_LINK,
+		.name = "I",
+	},
+	{
+		.index = SHF_LINK_ORDER,
+		.name = "L",
+	},
+	{
+		.index = SHF_OS_NONCONFORMING,
+		.name = "O",
+	},
+	{
+		.index = SHF_GROUP,
+		.name = "G",
+	},	
+	{
+		.index = SHF_TLS,
+		.name = "T",
+	},	
+	{
+		.index = SHF_COMPRESSED,
+		.name = "C",
+	},
+	{
+		.index = SHF_MASKOS,
+		.name = "+MO+",
+	},	
+	{
+		.index = SHF_MASKPROC,
+		.name = "+MASKPROC+",
+	},
+	{
+		.index = SHF_ORDERED,
+		.name = "+ORDERED+",
+	},
+	{
+		.index = SHF_EXCLUDE,
+   		.name = "+EXCLUDE+",
+	},
+	{}
+};
+
+
+#define sh_flags(sh_flags) ({\
+			int h = 0,s_offset = 0,size_array = sizeof(sh_flags_index)/sizeof(sh_flags_index[0]),i = 0; \
+			char buf[1024];\
+			for(i = 0;i < size_array;i++)\
+				if(sh_flags & sh_flags_index[i].index){\
+				     strcpy(buf + s_offset,sh_flags_index[i].name);\
+				     s_offset +=strlen(sh_flags_index[i].name);\
+				     h++;\
+				     }\
+			(h == 0) ? "" : buf\
+			;})
 
 static void print_shdr(void **buf,Elf64_Ehdr *ehdr)
 {
